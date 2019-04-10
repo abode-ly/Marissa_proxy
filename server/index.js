a3d6c958a8f6d6ff3fd818412f4f5cd46b01f474
Marissa <marissa.shieh@gmail.com>
2019-04-10 15:29:59 -0700

    Render initial display of all components

--- server/index.js
+++ server/index.js
@@ -6,17 +6,11 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname,'../public')));
 
-// app.get('/photosandcomments/:id', (req, res) => {
-//   axios.get(`http://localhost:3001/photosandcomments/${req.params.id}`)
-//     .then((response) => res.send(response.data) );
-// })
-
-// app.get('/abodes/:abode_id/reviews', (req, res) => {
-//   // console.log(req.params.id);
-//   axios.get(`http://localhost:3002/abodes/${req.params.abode_id}/reviews`)
-//     .then(response => res.send(response.data))
-//     .catch(err => res.status(404).end(err));
-// })
+app.get('/photosandcomments/:id', (req, res) => {
+  axios.get(`http://localhost:3001/photosandcomments/${req.params.id}`)
+    .then((response) => res.send(response.data) )
+    .catch(err => res.status(404).end(err));
+})
 
 app.get('/abodes/:abode_id/reviews', (req, res) => {
   // console.log(req.params.id);
@@ -25,6 +19,23 @@ app.get('/abodes/:abode_id/reviews', (req, res) => {
     .catch(err => res.status(404).end(err));
 })
 
+app.get('/homes', (req, res) => {
+  axios.get(`http://localhost:3004/homes`)
+    .then(response => res.send(response.data))
+    .catch(err => res.status(404).end(err));
+})
+
+app.get('/bookings/:id/reserve', (req, res) => {
+  axios.get(`http://localhost:3003/bookings/${req.params.id}/reserve`)
+    .then(response => res.send(response.data))
+    .catch(err => res.status(404).end(err));
+})
+
+app.get('/bookings/:accommodationID/reserve/:startDate&:endDate', (req, res) => {
+  axios.get(`http://localhost:3003/bookings/${req.params.accommodationID}/reserve/${req.params.startDate}&${req.params.endDate}`)
+    .then(response => res.send(response.data))
+    .catch(err => res.status(404).end(err));
+})
 
 {/* <script src="http://localhost:3001/carousel.app.js"></script>
 <script src="http://localhost:3002/app.js"></script>
