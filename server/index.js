c5b85bd203b37d040f896cc521d6f755d84efc54
Marissa <marissa.shieh@gmail.com>
2019-04-10 12:33:25 -0700

    Render reviews on proxy

--- server/index.js
+++ server/index.js
@@ -1,14 +1,37 @@
 const express = require('express');
-// const path = require('path');
+const axios = require('axios');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'../public')));
 
-app.get('/:id', (req, res) => {
+// console.log(window.location);
+
+// app.get('/:id', (req, res) => {
+
+// })
+
+
+app.get('/abodes/:abode_id/reviews', (req, res) => {
   console.log(req.params.id);
+  axios.get(`http://localhost:3002/abodes/${req.params.abode_id}/reviews`)
+    .then(response => res.send(response.data))
+    .catch(err => res.status(404).end(err));
 })
 
+// app.get('http://localhost:3001/photosandcomments/:id', (req, res) => {
+//   console.log(req.params.id);
+//   axios.get(`http://localhost:3001/photosandcomments/${req.params.id}`)
+//     .then((response) => console.log(response) );
+// })
+
+{/* <script src="http://localhost:3001/carousel.app.js"></script>
+<script src="http://localhost:3002/app.js"></script>
+<script src="http://localhost:3003/BookingModule.bundle.js"></script>
+<script src="http://localhost:3004/nearbyHomes.app.js"></script> */}
+
 app.listen(port, () => {
+  console.log('in the server');
   console.log(`Server listening at http://localhost: ${port}`);
 });
