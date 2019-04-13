b329f8b8b17976f22ec6b1e81a3aef24d17cad20
Marissa <marissa.shieh@gmail.com>
2019-04-13 14:34:16 -0700

    Set up docker to create image

--- server/index.js
+++ server/index.js
@@ -11,31 +11,31 @@ app.get('/:path', (req, res) => {
 });
 
 app.get('/photosandcomments/:id', (req, res) => {
-  axios.get(`http://localhost:3001/photosandcomments/${req.params.id}`)
+  axios.get(`http://54.152.254.52/photosandcomments/${req.params.id}`)
   .then((response) => res.send(response.data) )
   .catch(err => res.status(404).end(err));
 })
 
 app.get('/abodes/:abode_id/reviews', (req, res) => {
-  axios.get(`http://localhost:3002/abodes/${req.params.abode_id}/reviews`)
+  axios.get(`http://18.144.44.107:3002/abodes/${req.params.abode_id}/reviews`)
     .then(response => res.send(response.data))
     .catch(err => res.status(404).end(err));
 })
 
 app.get('/similarhomes/:host_id/nearby', (req, res) => {
-  axios.get(`http://localhost:3004/similarhomes/${req.params.host_id}/nearby`)
+  axios.get(`http://18.191.149.200:3004/similarhomes/${req.params.host_id}/nearby`)
     .then(response => res.send(response.data))
     .catch(err => res.status(404).end(err));
 })
 
 app.get('/bookings/:id/reserve', (req, res) => {
-  axios.get(`http://localhost:3003/bookings/${req.params.id}/reserve`)
+  axios.get(`http://ec2-18-218-39-128.us-east-2.compute.amazonaws.com:3003/bookings/${req.params.id}/reserve`)
     .then(response => res.send(response.data))
     .catch(err => res.status(404).end(err));
 })
 
 app.get('/bookings/:accommodationID/reserve/:startDate&:endDate', (req, res) => {
-  axios.get(`http://localhost:3003/bookings/${req.params.accommodationID}/reserve/${req.params.startDate}&${req.params.endDate}`)
+  axios.get(`http://ec2-18-218-39-128.us-east-2.compute.amazonaws.com:3003/bookings/${req.params.accommodationID}/reserve/${req.params.startDate}&${req.params.endDate}`)
     .then(response => res.send(response.data))
     .catch(err => res.status(404).end(err));
 })
